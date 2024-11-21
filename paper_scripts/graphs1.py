bcfa9f24b55f74c97b8a51d42648c7cec56ba4d6
Jakub Kopal <kopal@login02.devana.local>
2024-11-21 17:39:39 +0100

    Nicer graphs

--- paper_scripts/graphs1.py
+++ paper_scripts/graphs1.py
@@ -10,59 +10,40 @@ import matplotlib.pyplot as plt
 
 
 
-
-optimizers_names_mapping = {
-    "sgd_baseline": "CM",
-    "nesterov": "NAG",
-    "sgd_overshoot_3": "SGD3",
-    "sgd_overshoot_5": "SGD5",
-    "sgd_overshoot_7": "SGD7",
-    "sgd_overshoot_adaptive": "SGDA",
-    "adam_baseline": "Adam",
-    "nadam": "Nadam",
-    "adam_overshoot_3": "Adam3",
-    "adam_overshoot_5": "Adam5",
-    "adam_overshoot_7": "Adam7",
-    "adam_overshoot_adaptive": "AdamA",
-}
+tasks_to_skip = [
+    "mlp_housing_v2"
+]
 
 task_name_mapping = {
-    "mlp_housing": "MLP Housing (loss)",
-    "mlp_mnist": "Mnist (acc)",
-    "2c2d_fashion": "2c2c F-MNIST (acc)",
-    "3c3d_cifar10": "3c3d CIFAR-10 (acc)",
-    "vae_mnist": "VAE MNIST (loss)",
-    "vae_f-mnist": "VAE F-MNIST (loss)",
+    "vae_mnist_replicate": "VAE-M",
+    "2c2d_fashion_replicate": "2c2d-FM",
+    "3c3d_cifar10_replicate": "3c3d-C10",
+    "resnet18_cifar100_v2_better_aug_weight_decay_new_sgd": "ResNet-C100",
 }
 
-rows_to_drop = [
-    "Mnist (acc)"
-]
-columns_to_drop = [
-    "SGDA",
-    "AdamA",
-]
-
-def bold_min(row):
-    # import code; code.interact(local=locals())
-    if "acc" in row.name:
-        fn_to_use = max
-    elif "loss" in row.name:
-        fn_to_use = min
-    else:
-        raise Exception(f"task name have to contain either acc or loss: {row.name}")
-        
-    sgd_min = fn_to_use([x[1][0] for x in row.items() if 'SGD' in x[0] or 'CM' in x[0]])
-    adam_min = fn_to_use([x[1][0] for x in row.items() if 'Adam' in x[0]])
-    return [f"\\textbf{{{val[0]:.2f} \u00B1{val[1]:.2f}}}" if (val[0] == sgd_min or val[0] == adam_min) else f"{val[0]:.2f} \u00B1{val[1]:.2f}" for _, val in row.items()]
+color_map = {
+    "sgd_baseline": [1, 0, 0],
+    "sgd_overshoot_3": [0.6, 0.4, 0],
+    "sgd_overshoot_5": [0.6, 0, 0.4],
+    "sgd_overshoot_7": [0.6, 0.2, 0.2],
+    "adam_baseline": [0, 0, 1],
+    "adam_overshoot_3": [0.4, 0, 0.6],
+    "adam_overshoot_5": [0, 0.4, 0.6],
+    "adam_overshoot_7": [0.2, 0.2, 0.6],
+}
 
 
-def plot_data(data, pp, title):
-    for label, loss in data.items():
-        pp.plot(loss, label=label)
-    pp.set_yscale("symlog")
+def plot_data(data, pp, title, use_legend=True):
+
+    for label, color in color_map.items():
+        line_style = 'dotted' if "baseline" in label else 'solid'
+        pp.plot(data[label], label=label, color=color, linestyle=line_style)
+
+    pp.set_yscale('log', base=10)
     pp.set_title(title)
-    pp.legend(loc="upper left")
+    if use_legend:
+        pp.legend(loc="upper left")
+        # pp.legend(color_map.keys(), loc="upper left",)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -72,16 +53,23 @@ if __name__ == "__main__":
 
     root = "../lightning_logs/table1"
     
-    fig, axs = plt.subplots(2, len(task_name_mapping.keys()), figsize=(52, 20))
+    fig, axs = plt.subplots(2, 3, figsize=(20, 10))
     all_results = {}
-    for task_index, task_name in enumerate(os.listdir(root)):
+
+
+    task_index = -1
+    for task_name in os.listdir(root):
         task_root = os.path.join(root, task_name)
+        if (not os.path.isdir(task_root)) or (task_name not in task_name_mapping):
+            continue
+        
+        print(f"Processing {task_name}")
+        task_index += 1
 
         if task_name == "mlp_mnist":
             continue
 
-        sgd_results = {}
-        adam_results = {}
+        task_results = {}
         for run_name in os.listdir(task_root):
             run_root = os.path.join(task_root, run_name)
             if not os.path.isdir(run_root):
@@ -94,30 +82,29 @@ if __name__ == "__main__":
                 finel_path = os.path.join(run_root, seed_id)
             
                 stats_path = os.path.join(finel_path, "training_stats.csv")
+                    
                 if not os.path.exists(stats_path):
                     continue
                 losses = pd.read_csv(stats_path)['base_loss_100']
                 if run_losses is None:
-                    run_losses = np.zeros((n_seeds, (len(losses) // 6) - 200))
-                run_losses[i] = losses[200:len(losses) // 6]
+                    run_losses = np.zeros((n_seeds, (len(losses) // 8)))
+                if "housing" in task_name:
+                    run_losses[i] = 100 * losses[:len(losses) // 8]
+                else:
+                    run_losses[i] = losses[:len(losses) // 8]
 
-            if run_losses is None or "adaptive" in run_name:
+            if run_losses is None or "adaptive" in run_name or "nesterov" in run_name or "nadam" in run_name:
                 continue
-            elif ("sgd" in run_name) or ("nesterov" in run_name):
-                sgd_results[run_name] = run_losses.mean(0)
             else:
-                adam_results[run_name] = run_losses.mean(0)
+                task_results[run_name] = run_losses.mean(0)
             
-            
-        if sgd_results:
-            plot_data(sgd_results, axs[0, task_index], task_name)
-        if adam_results:
-            plot_data(adam_results, axs[1, task_index], task_name)
+        if task_results:
+            plot_data(task_results, axs[task_index % 2, task_index // 2], task_name, task_index == 0)
 
         print(f"Updated with {task_name}")
         fig.suptitle('SGD', fontsize=16)
         plt.tight_layout()
-        plt.savefig('foo.png')
+        plt.savefig('foo_test.png')
             
 
 
