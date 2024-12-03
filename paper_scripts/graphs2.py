800fe91de3be6837a707988ef8f2fc4ef09dfa83
kopalja <kopal.jakub@gmail.com>
2024-12-03 16:37:14 +0100

    Multiple betas for graph2

--- paper_scripts/graphs2.py
+++ paper_scripts/graphs2.py
@@ -24,28 +24,30 @@ task_2_title = {
 
 
 
-def plot_data(data, pp, title):
+def plot_data(data_90, data_95, task_index, title):
 
-    x_coords = [x[0] for x in data]
-    y_coords = [x[1] for x in data]
-    values = [x[2] for x in data]
+    for i, data in enumerate([data_90, data_95]):
+        pp = axs[i, task_index]
+        x_coords = [x[0] for x in data]
+        y_coords = [x[1] for x in data]
+        values = [x[2] for x in data]
 
-     
-    scatter = pp.scatter(x_coords, y_coords, c=values, cmap="viridis", s=500)
-    # scatter = pp.scatter(x_coords, y_coords, c=values, cmap="gray", s=240)
+        
+        scatter = pp.scatter(x_coords, y_coords, c=values, cmap="viridis", s=500)
+        # scatter = pp.scatter(x_coords, y_coords, c=values, cmap="gray", s=240)
 
-    cbar = fig.colorbar(scatter, ax=pp, orientation="horizontal", label="Train loss", location='top', pad=0.11, format="{x:.2f}")
-    # cbar = fig.colorbar(scatter, ax=pp, orientation="horizontal", label="Train loss", location='top', pad=0.12)
+        cbar = fig.colorbar(scatter, ax=pp, orientation="horizontal", label="Train loss", location='top', pad=0.13, format="{x:.2f}")
 
-    pp.set_title(title, fontsize=28, pad=100)
-    cbar.set_label("Train loss", fontsize=24)
-    
-    cbar.ax.tick_params(labelsize=18)
-    pp.xaxis.set_tick_params(labelsize=18)
-    pp.yaxis.set_tick_params(labelsize=18)
-    
-    cbar.ax.xaxis.set_label_position('bottom')
-    cbar.ax.xaxis.set_ticks_position('bottom')
+        if i == 0:
+            pp.set_title(title, fontsize=28, pad=100)
+        cbar.set_label("Train loss", fontsize=24)
+        
+        cbar.ax.tick_params(labelsize=18)
+        pp.xaxis.set_tick_params(labelsize=18)
+        pp.yaxis.set_tick_params(labelsize=18)
+        
+        cbar.ax.xaxis.set_label_position('bottom')
+        cbar.ax.xaxis.set_ticks_position('bottom')
 
 
 
@@ -86,13 +88,6 @@ def process_run(run_root):
     # import code; code.interact(local=locals())
     return np.mean(run_distances), np.mean(run_losses)
 
-    if run_losses:
-        min_len = min([len(x) for x in run_losses])
-        return np.array([loss[:min_len] for loss in run_losses]).mean(0)
-    else:
-        print(f"Warning, no losses for {run_root}")
-        return None
-
         
 
 if __name__ == "__main__":
@@ -103,33 +98,40 @@ if __name__ == "__main__":
 
     plt.rc('xtick', labelsize=14)  # X-axis tick labels font size
     plt.rc('ytick', labelsize=14)  # Y-axis tick labels font size
-    fig, axs = plt.subplots(1, 6, figsize=(40, 8))
+    fig, axs = plt.subplots(2, 6, figsize=(40, 14))
     fig.text(0.5, 0.01, 'Overshoot factor', ha='center', fontsize=40)
-    fig.text(0.01, 0.5, 'Average weighted distance', va='center', rotation='vertical', fontsize=34)
+    fig.text(0.03, 0.5, 'Average weighted distance', va='center', rotation='vertical', fontsize=34)
+    fig.text(0.01, 0.25, r"$\beta$1 = 0.95", va='center', rotation='vertical', fontsize=30)
+    fig.text(0.01, 0.7, r"$\beta$1 = 0.9", va='center', rotation='vertical', fontsize=30)
     all_results = {}
 
 
     task_index = -1
     for task_name in [task_name for task_name in task_2_title if os.path.isdir(os.path.join(args.root, task_name))]:
         task_root = os.path.join(args.root, task_name)
+        task_root_95 = os.path.join(args.root, f"{task_name}_95")
         print(f"Processing {task_name}")
         task_index += 1
-        task_results = []
+        task_results, task_results_95 = [], []
         for run_name in os.listdir(task_root):
-            # if '15' in run_name or '14' in run_name or '13' in run_name:
-            #     continue
             run_root = os.path.join(task_root, run_name)
             if os.path.isdir(run_root):
                 distance, loss = process_run(run_root)
-                # task_results.append((int(run_name.split('_')[-1]), distance, loss))
                 task_results.append((float(run_name.split('_')[-1]), distance, loss))
+                
+        if os.path.isdir(task_root_95):
+            for run_name in os.listdir(task_root_95):
+                run_root = os.path.join(task_root_95, run_name)
+                if os.path.isdir(run_root):
+                    distance, loss = process_run(run_root)
+                    task_results_95.append((float(run_name.split('_')[-1]), distance, loss))
 
 
         # plot_data(task_results, axs[task_index % 2, task_index // 2], task_2_title[task_name])
-        plot_data(task_results, axs[task_index], task_2_title[task_name])
+        plot_data(task_results, task_results_95, task_index, task_2_title[task_name])
 
 
         print(f"Updated with {task_name}")
-        plt.tight_layout(rect=[0.02, 0.07, 1, 1])  # Leave space for the global labels
+        plt.tight_layout(rect=[0.04, 0.07, 1, 1])  # Leave space for the global labels
         plt.savefig(args.out)
             
