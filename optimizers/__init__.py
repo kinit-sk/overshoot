ac9cdef7b6050b68123f5b9cefe7ca0eebc0a72c
kopalja <kopal.jakub@gmail.com>
2024-12-09 16:29:51 +0100

    Implement AdamO for multitensors

--- optimizers/__init__.py
+++ optimizers/__init__.py
@@ -5,7 +5,7 @@ from optimizers.sgdo_adaptive import SGDO as SGDO_adaptive
 from optimizers.adamw_overshoot_replication import AdamW as OvershootAdamW_replication
 from optimizers.adamw_overshoot_full_approximation import AdamW as OvershootAdamW_full_approximation
 from optimizers.adamw_overshoot_denom_approximation import AdamW as OvershootAdamW_denom_approximation
-from optimizers.adamw_overshoot_delayed import AdamW as OvershootAdamW_delayed
+from optimizers.adamw_overshoot_delayed import AdamO as OvershootAdamW_delayed
 from optimizers.adamw_overshoot_adaptive import AdamW as OvershootAdamW_adaptive
 
 optimizers_map = {
@@ -29,7 +29,8 @@ optimizers_map = {
 
 
 
-def create_optimizer(opt_name, param_groups, overshoot_factor, lr, config):
+def create_optimizer(opt_name, param_groups, overshoot_factor, lr, config, foreach=None):
+    foreach = True
     if opt_name == "nadam":
         opt = optimizers_map[opt_name](
             param_groups,
@@ -38,7 +39,7 @@ def create_optimizer(opt_name, param_groups, overshoot_factor, lr, config):
             momentum_decay=1000000000000000000000000, # Turn of momentum decay
             weight_decay=config.weight_decay,
             decoupled_weight_decay=True,
-            foreach=False,
+            foreach=foreach,
         )
     elif opt_name == "adamW_overshoot_delayed":
         opt = optimizers_map[opt_name](
@@ -48,7 +49,7 @@ def create_optimizer(opt_name, param_groups, overshoot_factor, lr, config):
             weight_decay=config.weight_decay,
             overshoot=overshoot_factor,
             overshoot_delay=config.overshoot_delay,
-            foreach=False,
+            foreach=foreach
         )
     elif opt_name == "adamW_overshoot_adaptive":
         opt = optimizers_map[opt_name](
@@ -57,7 +58,7 @@ def create_optimizer(opt_name, param_groups, overshoot_factor, lr, config):
             betas=(config.adam_beta1, config.adam_beta2),
             weight_decay=config.weight_decay,
             cosine_target=config.target_cosine_similarity,
-            foreach=False,
+            foreach=foreach
         )
     elif opt_name.startswith("adamW_overshoot"):
         opt = optimizers_map[opt_name](
@@ -66,7 +67,7 @@ def create_optimizer(opt_name, param_groups, overshoot_factor, lr, config):
             betas=(config.adam_beta1, config.adam_beta2),
             weight_decay=config.weight_decay,
             overshoot=overshoot_factor,
-            foreach=False,
+            foreach=foreach,
         )
     elif "adam" in opt_name:
         config.adam_beta1 *= "zero" not in opt_name
@@ -75,6 +76,7 @@ def create_optimizer(opt_name, param_groups, overshoot_factor, lr, config):
             lr=lr,
             betas=(config.adam_beta1, config.adam_beta2),
             weight_decay=config.weight_decay,
+            foreach=foreach,
         )
     elif "sgd_adaptive" in opt_name:
         opt = optimizers_map[opt_name](
@@ -83,7 +85,7 @@ def create_optimizer(opt_name, param_groups, overshoot_factor, lr, config):
             momentum=config.sgd_momentum,
             weight_decay=config.weight_decay,
             cosine_target=config.target_cosine_similarity,
-            foreach=False,
+            foreach=foreach,
         )
     elif "sgd_overshoot" in opt_name:
         opt = optimizers_map[opt_name](
@@ -92,7 +94,7 @@ def create_optimizer(opt_name, param_groups, overshoot_factor, lr, config):
             momentum=config.sgd_momentum,
             weight_decay=config.weight_decay,
             overshoot=overshoot_factor,
-            foreach=False,
+            foreach=foreach,
         )
     elif "sgd" in opt_name:
         opt = optimizers_map[opt_name](
@@ -101,7 +103,7 @@ def create_optimizer(opt_name, param_groups, overshoot_factor, lr, config):
             momentum=0 if opt_name == "sgd" else config.sgd_momentum,
             weight_decay=config.weight_decay,
             nesterov="nesterov" in opt_name,
-            foreach=False,
+            foreach=foreach,
         )
     else:
         raise Exception(f"Optimizer {opt_name} not recognized.")
--- optimizers/adamw_overshoot_delayed.py
+++ optimizers/adamw_overshoot_delayed.py
@@ -24,10 +24,10 @@ from torch.optim.optimizer import (
     ParamsT,
 )
 
-__all__ = ["AdamW", "adamw"]
+__all__ = ["AdamO", "adamo"]
 
 
-class AdamW(Optimizer):
+class AdamO(Optimizer):
     def __init__(
         self,
         params: ParamsT,
@@ -35,8 +35,8 @@ class AdamW(Optimizer):
         betas: Tuple[float, float] = (0.9, 0.999),
         eps: float = 1e-8,
         weight_decay: float = 1e-2,
-        overshoot: float = 0,
-        overshoot_delay: int = 0,
+        overshoot: float = 5,
+        overshoot_delay: int = 50,
         amsgrad: bool = False,
         *,
         maximize: bool = False,
@@ -139,7 +139,7 @@ class AdamW(Optimizer):
             has_complex |= torch.is_complex(p)
             params_with_grad.append(p)
             if p.grad.is_sparse:
-                raise RuntimeError("AdamW does not support sparse gradients")
+                raise RuntimeError("AdamO does not support sparse gradients")
             grads.append(p.grad)
 
             state = self.state[p]
@@ -230,7 +230,7 @@ class AdamW(Optimizer):
                 state_steps,
             )
 
-            adamw(
+            adamo(
                 params_with_grad,
                 grads,
                 exp_avgs,
@@ -288,76 +288,8 @@ class AdamW(Optimizer):
                         denom = (self.state[param]["exp_avg_sq"].sqrt() / (1 - beta2**step)**0.5).add_(group["eps"])
                         param.addcdiv_(self.state[param]["exp_avg"], denom, value=-group["lr"] * overshoot / (1 - beta1**step))
 
-AdamW.__doc__ = (
-    r"""Implements AdamW algorithm.
 
-    .. math::
-       \begin{aligned}
-            &\rule{110mm}{0.4pt}                                                                 \\
-            &\textbf{input}      : \gamma \text{(lr)}, \: \beta_1, \beta_2
-                \text{(betas)}, \: \theta_0 \text{(params)}, \: f(\theta) \text{(objective)},
-                \: \epsilon \text{ (epsilon)}                                                    \\
-            &\hspace{13mm}      \lambda \text{(weight decay)},  \: \textit{amsgrad},
-                \: \textit{maximize}                                                             \\
-            &\textbf{initialize} : m_0 \leftarrow 0 \text{ (first moment)}, v_0 \leftarrow 0
-                \text{ ( second moment)}, \: \widehat{v_0}^{max}\leftarrow 0              \\[-1.ex]
-            &\rule{110mm}{0.4pt}                                                                 \\
-            &\textbf{for} \: t=1 \: \textbf{to} \: \ldots \: \textbf{do}                         \\
-
-            &\hspace{5mm}\textbf{if} \: \textit{maximize}:                                       \\
-            &\hspace{10mm}g_t           \leftarrow   -\nabla_{\theta} f_t (\theta_{t-1})          \\
-            &\hspace{5mm}\textbf{else}                                                           \\
-            &\hspace{10mm}g_t           \leftarrow   \nabla_{\theta} f_t (\theta_{t-1})           \\
-            &\hspace{5mm} \theta_t \leftarrow \theta_{t-1} - \gamma \lambda \theta_{t-1}         \\
-            &\hspace{5mm}m_t           \leftarrow   \beta_1 m_{t-1} + (1 - \beta_1) g_t          \\
-            &\hspace{5mm}v_t           \leftarrow   \beta_2 v_{t-1} + (1-\beta_2) g^2_t          \\
-            &\hspace{5mm}\widehat{m_t} \leftarrow   m_t/\big(1-\beta_1^t \big)                   \\
-            &\hspace{5mm}\widehat{v_t} \leftarrow   v_t/\big(1-\beta_2^t \big)                   \\
-            &\hspace{5mm}\textbf{if} \: amsgrad                                                  \\
-            &\hspace{10mm}\widehat{v_t}^{max} \leftarrow \mathrm{max}(\widehat{v_t}^{max},
-                \widehat{v_t})                                                                   \\
-            &\hspace{10mm}\theta_t \leftarrow \theta_t - \gamma \widehat{m_t}/
-                \big(\sqrt{\widehat{v_t}^{max}} + \epsilon \big)                                 \\
-            &\hspace{5mm}\textbf{else}                                                           \\
-            &\hspace{10mm}\theta_t \leftarrow \theta_t - \gamma \widehat{m_t}/
-                \big(\sqrt{\widehat{v_t}} + \epsilon \big)                                       \\
-            &\rule{110mm}{0.4pt}                                                          \\[-1.ex]
-            &\bf{return} \:  \theta_t                                                     \\[-1.ex]
-            &\rule{110mm}{0.4pt}                                                          \\[-1.ex]
-       \end{aligned}
-
-    For further details regarding the algorithm we refer to `Decoupled Weight Decay Regularization`_.
-    """
-    + rf"""
-    Args:
-        params (iterable): iterable of parameters to optimize or dicts defining
-            parameter groups
-        lr (float, Tensor, optional): learning rate (default: 1e-3). A tensor LR
-            is not yet supported for all our implementations. Please use a float
-            LR if you are not also specifying fused=True or capturable=True.
-        betas (Tuple[float, float], optional): coefficients used for computing
-            running averages of gradient and its square (default: (0.9, 0.999))
-        eps (float, optional): term added to the denominator to improve
-            numerical stability (default: 1e-8)
-        weight_decay (float, optional): weight decay coefficient (default: 1e-2)
-        amsgrad (bool, optional): whether to use the AMSGrad variant of this
-            algorithm from the paper `On the Convergence of Adam and Beyond`_
-            (default: False)
-        {_maximize_doc}
-        {_foreach_doc}
-        {_capturable_doc}
-        {_differentiable_doc}
-        {_fused_doc}
-    .. _Decoupled Weight Decay Regularization:
-        https://arxiv.org/abs/1711.05101
-    .. _On the Convergence of Adam and Beyond:
-        https://openreview.net/forum?id=ryQu7f-RZ
-
-    """
-)
-
-
-def _single_tensor_adamw(
+def _single_tensor_adamo(
     params: List[Tensor],
     grads: List[Tensor],
     exp_avgs: List[Tensor],
@@ -421,7 +353,7 @@ def _single_tensor_adamw(
         exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
 
         if capturable or differentiable:
-            raise Exception("Not implemented")
+            raise Exception("AdamO for caprutable not implemented.")
             step = step_t
 
             bias_correction1 = 1 - beta1**step
@@ -483,8 +415,7 @@ def _single_tensor_adamw(
             max_exp_avg_sqs[i] = torch.view_as_complex(max_exp_avg_sqs[i])
 
 
-# TODO: Not implemented yet
-def _multi_tensor_adamw(
+def _multi_tensor_adamo(
     params: List[Tensor],
     grads: List[Tensor],
     exp_avgs: List[Tensor],
@@ -507,7 +438,6 @@ def _multi_tensor_adamw(
     differentiable: bool,
     has_complex: bool,
 ):
-    raise Exception("Not implemented")
     if len(params) == 0:
         return
 
@@ -597,7 +527,7 @@ def _multi_tensor_adamw(
         bias_correction2_sqrt: Union[Tuple[Tensor, ...], List[Tensor]]
 
         if capturable:
-            raise Exception("AdamW overshoot for multitensor caprutable not implemented.")
+            raise Exception("AdamO for caprutable not implemented.")
             bias_correction1 = torch._foreach_pow(beta1, device_state_steps)
             bias_correction2 = torch._foreach_pow(beta2, device_state_steps)
             # foreach_sub doesn't allow a scalar as the first arg
@@ -636,9 +566,9 @@ def _multi_tensor_adamw(
             # at this point, exp_avg_sq_sqrt = - (1 - beta^t) * [sqrt(exp_avg_sq / (1 - beta2^t)) + eps] / lr
             torch._foreach_addcdiv_(device_params, device_exp_avgs, exp_avg_sq_sqrt)
         else:
-            # bias_correction1 = [
-            #     1 - beta1 ** _get_value(step) for step in device_state_steps
-            # ]
+            bias_correction1 = [
+                1 - beta1 ** _get_value(step) for step in device_state_steps
+            ]
             bias_correction2 = [
                 1 - beta2 ** _get_value(step) for step in device_state_steps
             ]
@@ -662,23 +592,44 @@ def _multi_tensor_adamw(
 
             torch._foreach_div_(exp_avg_sq_sqrt, bias_correction2_sqrt)
             torch._foreach_add_(exp_avg_sq_sqrt, eps)
+
+            # OLD
+            # torch._foreach_addcdiv_(
+            #     device_params,
+            #     device_exp_avgs,
+            #     exp_avg_sq_sqrt,
+            #     step_size,  # type: ignore[arg-type]
+            # )
+
+            # NEW
+            clamp = lambda x_list, l, h: [max(min(x, h), l) for x in x_list]
+            overshoot_old = clamp([step - 1 - overshoot_delay for step in device_state_steps], 0, overshoot)
+            overshoot_new = clamp([step - overshoot_delay for step in device_state_steps], 0, overshoot)
+
+            # 1) Multiply gradinets
+            torch._foreach_mul_(
+                device_grads,
+                _stack_if_compiling([(-lr / bc) * overshoot * (1-beta1) / beta1 for bc, overshoot in zip(bias_correction1, overshoot_old)]),
+            )
             
+            # 2) Add momenutm multiplication
+            torch._foreach_add_(
+                device_grads,
+                torch._foreach_mul(
+                    device_exp_avgs,
+                    _stack_if_compiling([(-lr / bc) * (o_new - o_old/beta1 + 1) for bc, o_old, o_new in zip(bias_correction1, overshoot_old, overshoot_new)]),
+                    )
+            )
+            
+            # 3) Divide by second moments
             torch._foreach_addcdiv_(
                 device_params,
                 device_grads,
                 exp_avg_sq_sqrt,
-                [-lr * (overshoot/beta1) * (1 - beta1) / (1 - beta1 ** _get_value(step)) for step in device_state_steps] # type: ignore[arg-type]
-            )
-            
-            torch._foreach_addcdiv_(
-                device_params,
-                device_exp_avgs,
-                exp_avg_sq_sqrt,
-                [-lr * (overshoot - overshoot/beta1 + 1) / (1 - beta1 ** (_get_value(step) + 1)) for step in device_state_steps] # type: ignore[arg-type]
             )
 
 
-def _fused_adamw(
+def _fused_adamo(
     params: List[Tensor],
     grads: List[Tensor],
     exp_avgs: List[Tensor],
@@ -701,6 +652,7 @@ def _fused_adamw(
     differentiable: bool,
     has_complex: bool,  # Needed for consistency.
 ) -> None:
+    raise Exception("Fused AdamO not implemented.")
     if not params:
         return
     if differentiable:
@@ -772,8 +724,8 @@ def _fused_adamw(
             )
 
 
-@_disable_dynamo_if_unsupported(single_tensor_fn=_single_tensor_adamw)
-def adamw(
+@_disable_dynamo_if_unsupported(single_tensor_fn=_single_tensor_adamo)
+def adamo(
     params: List[Tensor],
     grads: List[Tensor],
     exp_avgs: List[Tensor],
@@ -800,10 +752,6 @@ def adamw(
     eps: float,
     maximize: bool,
 ):
-    r"""Functional API that performs AdamW algorithm computation.
-
-    See :class:`~torch.optim.AdamW` for details.
-    """
     if not torch._utils.is_compiling() and not all(
         isinstance(t, torch.Tensor) for t in state_steps
     ):
@@ -833,11 +781,11 @@ def adamw(
         raise RuntimeError("torch.jit.script not supported with fused optimizers")
 
     if fused and not torch.jit.is_scripting():
-        func = _fused_adamw
+        func = _fused_adamo
     elif foreach and not torch.jit.is_scripting():
-        func = _multi_tensor_adamw
+        func = _multi_tensor_adamo
     else:
-        func = _single_tensor_adamw
+        func = _single_tensor_adamo
 
     func(
         params,
