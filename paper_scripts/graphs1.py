b836d0bcd458d7919a5f14012b07ca38ed1ea828
kopalja <kopal.jakub@gmail.com>
2024-12-14 20:58:59 +0100

    Generate test loss

--- paper_scripts/graphs1.py
+++ paper_scripts/graphs1.py
@@ -25,14 +25,25 @@ task_2_title = {
 }
 
 
-task_2_range = {
+
+task_2_range_train = {
     "mlp_housing": (0.1, 1),
     "vae_f-mnist": (22, 30),
     "vae_mnist": (26, 60),
     "2c2d_f-mnist": (0.0, 2),
     "3c3d_cifar10": (0.25, 4),
     "resnet18_cifar100": (0.0, 4),
-    # "gpt_hf_qqp": (0.35, 1),
+    "gpt_hf_qqp": (0.35, 1),
+}
+
+task_2_range_test = {
+    "mlp_housing": (0.25, 0.5),
+    "vae_f-mnist": (22, 20),
+    "vae_mnist": (26, 40),
+    "2c2d_f-mnist": (0.22, 1),
+    "3c3d_cifar10": (0.35, 3),
+    "resnet18_cifar100": (1.7, 4),
+    "gpt_hf_qqp": (0.28, 0.38),
 }
 
 task_2_smooth = {
@@ -71,6 +82,7 @@ algorithm_2_legend = {
 def plot_data(data, pp, task_name, use_legend=True):
 
     pp.set_yscale('log', base=10)
+    task_2_range = task_2_range_test if args.loss_type == "test" else task_2_range_train
     min_max = task_2_range[task_name]
     for label, color in color_map.items():
         if not label in data:
@@ -79,8 +91,13 @@ def plot_data(data, pp, task_name, use_legend=True):
         pp.plot(data[label] - min_max[0], label=algorithm_2_legend[label], color=color, linestyle=line_style, linewidth=1.5)
         
     pp.set_ylim([0, min_max[1]])
-    pp.set_yticks(pp.get_yticks()[-4:-2], [round(min_max[0]+c, 2) for c in pp.get_yticks()[-4:-2]])
-    pp.xaxis.set_major_formatter(FuncFormatter(lambda x, pos: f"{x * 10:.0f}"))
+    if "resnet" in task_name:
+        pp.set_yticks(pp.get_yticks()[-5:-2], [round(min_max[0]+c, 2) for c in pp.get_yticks()[-5:-2]])
+    else:
+        pp.set_yticks(pp.get_yticks()[-4:-2], [round(min_max[0]+c, 2) for c in pp.get_yticks()[-4:-2]])
+
+    if args.loss_type != "test":
+        pp.xaxis.set_major_formatter(FuncFormatter(lambda x, pos: f"{x * 10:.0f}"))
     pp.xaxis.set_major_locator(MaxNLocator(nbins=3)) 
     pp.set_title(task_2_title[task_name], fontsize=20)
     
@@ -92,18 +109,23 @@ def process_run(run_root, smooth_factor):
     run_losses = []
     for seed_id in os.listdir(run_root):
         finel_path = os.path.join(run_root, seed_id)
-        stats_path = os.path.join(finel_path, "training_stats.csv")
+        if args.loss_type == "test":
+            stats_path = os.path.join(finel_path, "test_stats.csv")
+            key = "loss"
+        else:
+            stats_path = os.path.join(finel_path, "training_stats.csv")
+            key = "overshoot_loss_1" if args.loss_type == "overshoot" else "base_loss_1"
+             
         if not os.path.exists(stats_path):
             print(f"Warning, file {stats_path} does not exists")
             continue
-        key = "overshoot_loss_1" if args.overshoot_loss else "base_loss_1"
         losses = pd.read_csv(stats_path, on_bad_lines='skip')[key].to_numpy()
-        # losses = pd.read_csv(stats_path, on_bad_lines='skip')['overshoot_loss_100'].to_numpy()
          
-        losses = losses[:(len(losses) // 10) * 10] # Make dividable by 20
-        losses = losses.reshape(-1, 10).mean(axis=1)
-        y_smooth = gaussian_filter1d(losses, smooth_factor)
-        losses = y_smooth
+        if args.loss_type != "test":
+            losses = losses[:(len(losses) // 10) * 10] # Make dividable by 20
+            losses = losses.reshape(-1, 10).mean(axis=1)
+            y_smooth = gaussian_filter1d(losses, smooth_factor)
+            losses = y_smooth
         run_losses.append(losses)
 
     if run_losses:
@@ -118,15 +140,20 @@ def process_run(run_root, smooth_factor):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--root", type=str, default="../lightning_logs/table1")
-    parser.add_argument("--overshoot_loss", action=argparse.BooleanOptionalAction, required=False)
-    parser.add_argument("--out", type=str, default="train_loss.png")
+    parser.add_argument("--loss_type", type=str, default="base")
     args = parser.parse_args()
+
+    assert args.loss_type in ["base", "overshoot", "test"]
 
     plt.rc('xtick', labelsize=14)  # X-axis tick labels font size
     plt.rc('ytick', labelsize=14)  # Y-axis tick labels font size
     fig, axs = plt.subplots(2, 3, figsize=(20, 10))
-    fig.text(0.5, 0.01, 'Training steps', ha='center', fontsize=22)
-    fig.text(0.01, 0.5, 'Training loss', va='center', rotation='vertical', fontsize=22)
+    if args.loss_type == "test":
+        fig.text(0.5, 0.01, 'Epochs', ha='center', fontsize=22)
+        fig.text(0.01, 0.5, 'Test loss', va='center', rotation='vertical', fontsize=22)
+    else:
+        fig.text(0.5, 0.01, 'Training steps', ha='center', fontsize=22)
+        fig.text(0.01, 0.5, 'Training loss', va='center', rotation='vertical', fontsize=22)
     all_results = {}
 
 
@@ -154,8 +181,5 @@ if __name__ == "__main__":
 
         print(f"Updated with {task_name}")
         plt.tight_layout(rect=[0.02, 0.04, 1, 1])  # Leave space for the global labels
-        if args.overshoot_loss:
-            plt.savefig(f"{args.out[:-4]}_overshoot.png")
-        else:
-            plt.savefig(args.out)
+        plt.savefig(f"graph1_{args.loss_type}_loss.png")
             
